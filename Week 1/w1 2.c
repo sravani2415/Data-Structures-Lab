af92cb600080fb0bb25626420a311cc4ad3efbde
sravani2415 <69664163+sravani2415@users.noreply.github.com>
2020-08-14 19:44:00 +0530

    Add files via upload

---
+++ Week 1/w1 2.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+struct student
+{
+        char name[20];
+        int roll;
+        int age;
+        char branch[3];
+        s[10];
+}
+int main()
+{
+        inti,n;
+        printf("enter number of students");
+        scanf("%d,&n);
+        for(i=0,i<n,i++)
+        {
+         printf("Enter Name: ");
+         scanf("%s",&s[i].name);
+         printf("Enter roll no: ");
+         scanf("%d",&s[i].roll);
+         printf("Enter age: ");
+         scanf("%d",&s[i].age);
+         printf("Enter branch: ");
+         scanf("%s",&s[i].branch);
+         }
+         for(int i=0,i<n,i++)
+         {
+          printf("Student Details : \n");
+          printf("name:");
+          puts(s[i].name);
+          printf("\nroll no:%d",s[i].roll);
+          printf("\nAge : %d",s[i].age);
+          printf("\Branch : ");
+          puts(s[i].branch);
+          }
+          return o;
+}
+     
+
---
+++ Week 1/w1 4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+           int data;
+           struct node*prev;
+           struct node*next;
+};
+
+int main()
+{         
+           struct node*s;
+           s=(struct node*)malloc(sixeof(struct node));
+           s->data=100;
+           s->prev=NULL;
+           s->next=NULL;
+           printf("data=%d",s->data);
+}
